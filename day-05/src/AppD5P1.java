3ea2c544b9b692c9f1fd0c4aae9866e6f9f755e1
Ramzza <thezniqer@gmail.com>
2024-10-25 19:36:29 +0300

    feat: d5 logic skeleton

--- day-05/src/AppD5P1.java
+++ day-05/src/AppD5P1.java
@@ -3,6 +3,8 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class AppD5P1 {
 
@@ -11,9 +13,27 @@ public class AppD5P1 {
 
     public static void main(String[] args) throws Exception {
         ArrayList<String> inputByLines = readInputFromFile(args);
-        boolean isTextSearchActive = args.length > 1 ? Boolean.parseBoolean(args[1]) : true;
 
-        System.out.println("Result: " + isTextSearchActive);
+        List<Integer> inputSeeds = getSeedsFromInput(inputByLines);
+        List<Map<Integer, MapEntry>> almanacMaps = getMapsFromInput(inputByLines);
+
+        int lowestLocation = inputSeeds.stream()
+                .map(seed -> getLocationForSeed(seed, almanacMaps))
+                .reduce(Integer.MAX_VALUE, Integer::min);
+
+        System.out.println("Lowest location number: " + lowestLocation);
+    }
+
+    private static int getLocationForSeed(int inputSeed, List<Map<Integer, MapEntry>> almanacMaps) {
+        return 0;
+    }
+
+    private static List<Map<Integer, MapEntry>> getMapsFromInput(ArrayList<String> inputLines) {
+        return new ArrayList<>();
+    }
+
+    private static List<Integer> getSeedsFromInput(ArrayList<String> inputByLines) {
+        return new ArrayList<>();
     }
 
     private static ArrayList<String> readInputFromFile(String[] args) {
@@ -34,5 +54,30 @@ public class AppD5P1 {
         }
 
         return resultFileByLines;
+    }
+
+    private class MapEntry {
+
+        private final int destination;
+        private final int source;
+        private final int range;
+
+        public MapEntry(int destination, int source, int range) {
+            this.destination = destination;
+            this.source = source;
+            this.range = range;
+        }
+
+        public int getDestination() {
+            return destination;
+        }
+
+        public int getSource() {
+            return source;
+        }
+
+        public int getRange() {
+            return range;
+        }
     }
 }
